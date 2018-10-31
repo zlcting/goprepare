c845b0a15f4a50b6b9d8efcb06cb91339811e686
zlcting <zlcting@gmail.com>
2018-10-31 20:07:41 +0800

    demo

--- demo/main.go
+++ demo/main.go
@@ -2,8 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 )
 
 func main() {
-	fmt.Println(111111)
+	var (
+		cmd    *exec.Cmd
+		err    error
+		output []byte
+	)
+	cmd = exec.Command("/bin/bash", "-c", "echo 1; echo 2")
+	output, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(output))
 }
